d352319b685426966e97b6d36bcc974aec9a3565
MasiaSaig <mullermaciej12@gmail.com>
2024-12-09 17:23:36 +0100

    Added timer increasing every 1ms, with delay function.

--- main.c
+++ main.c
@@ -4,22 +4,21 @@ TODO:
 	- configure pwm 
 	- to test PWM, configure led
 	- improve timer and delay function
+	
+Microprocessor specs:
+	CCLK = 100Mhz and PCLK = 25Mhz
 */
 
 #include "UART.h"
 #include "I2C_TMP2.h"
 #include "PWM.h"
+#include "timer.h"
+
 #include <LPC17xx.h>
 #include <PIN_LPC17xx.h>
 #include <Driver_I2C.h>
 #include <Board_LED.h>
 
-// TODO: temporary delay (delete later)
-volatile uint32_t msTicks = 0;		/* Variable to store millisecond ticks */
-/* See startup file startup_LPC17xx.s for SysTick vector */  
-void SysTick_Handler(void) { msTicks++; }
-// wait 'time' miliseconds
-void delay(unsigned int time) { msTicks=0; while(msTicks < time ) {} }
 
 int main(){
 	LED_Initialize();
@@ -42,7 +41,7 @@ int main(){
 	while(true){
 		// LPC_UART0->THR is a memory address, holding 8bit character, which is going to be send
 		// LPC_UART0->THR = 'C';										// sending char
-		// UARTsendString("AGH WFIS SW LAB TRIAL\t");		// sending string
+		// UARTsendString("AGH WFIS SW.\t");				// sending string
 		delay(500);
 		I2C_Event = 0U;	// clear event
 		I2Cdrv->MasterReceive(TMP2_ADDRESS, data, dataSize, false); 
---
+++ timer.h
@@ -0,0 +1,7 @@
+#ifndef _REGULATOR_TIMER_H_
+#define _REGULATOR_TIMER_H_
+
+void initTimer();
+void delay(unsigned int time_ms);
+
+#endif
